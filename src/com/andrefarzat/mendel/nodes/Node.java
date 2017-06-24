3dc66bc80fc3a6330d675eedc1569b725ff628f0
Andre Farzat <andrefarzat@gmail.com>
2017-06-24 12:42:16 -0300

    Cleaning up everything

--- src/com/andrefarzat/mendel/nodes/Node.java
+++
@@ -1,8 +0,0 @@
-package com.andrefarzat.mendel.nodes;
-
-public interface Node {
-    Node clone();
-    void mutate();
-    String toString();
-    double getValue(double value);
-}
--- src/com/andrefarzat/mendel/selectors/Selector.java
+++
@@ -1,8 +0,0 @@
-package com.andrefarzat.mendel.selectors;
-
-import com.andrefarzat.mendel.Population;
-
-
-public interface Selector {
-    Selection get(int i, int size, Population population);
-}
